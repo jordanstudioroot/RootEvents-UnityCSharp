6386b628f4d5a1d9f876387be920f838db51af61
Jordan Nelson <jordannelson@protonmail.com>
2020-02-18 11:09:20 -0600

    update package name

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,12 +2,20 @@ var mkdirp = require('mkdirp');
 var path = require('path');
 var ncp = require('ncp');
 
+let packageName = '';
+let args = process.argv.slice(2);
+
 // Package name
-var package = "RootEvents-UnityCSharp-NPM";
+if (args[0]) {
+  packageName = args[0];
+}
+else {
+  throw new Error("Missing arg to postinstall.js in package.json.");
+}
 
 // Paths
-var src = path.join(__dirname, '..', 'Assets', package);
-var dir = path.join(__dirname, '..', '..', '..', 'Assets', 'pkg-all', package);
+var src = path.join(__dirname, '..', 'Assets', packageName);
+var dir = path.join(__dirname, '..', '..', '..', 'Assets', 'pkg-all', packageName);
 
 // Create folder if missing
 mkdirp(dir, function (err) {
